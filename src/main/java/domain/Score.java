24860041c9296f63cdc6313ceed745361fb5d69e
SGKIM94 <tkdrn8578@naver.com>
2019-06-22 15:24:39 +0900

    [ADD] 볼링게임의 점수를 가지는 Score 일급 콜렉션을 추가
     * 사용자에게 입력받은 점수를 저장하는 일급 콜렉션 추가
     * inputScore() : 입력받은 점수가 정상인지 체크
     * checkScoreIsNull() : 점수가 Null 값인지 검사
     * checkScoreIsWrong() : 점수가 올바르지 않은 값인지 확인
     * equals() : equals 를 오버로딩 한 메서드로 score 멤버변수와 같은지 비교해주는 함수

---
+++ src/main/java/domain/Score.java
@@ -0,0 +1,62 @@
+package domain;
+
+import java.util.Objects;
+
+public class Score {
+    public static final int MINIMUM_SCORE = 0;
+    public static final int MAX_SCORE = 10;
+    public static final int STRIKE_SCORE = 10;
+    private int score;
+
+    public Score(int score) {
+        this.score = score;
+        inputScore();
+    }
+
+    public Score(String score) {
+        this(Integer.parseInt(score));
+    }
+
+    public void inputScore() {
+        checkScoreIsNull();
+        checkScore();
+    }
+
+    private void checkScoreIsNull() {
+        if (String.valueOf(this.score).isEmpty()) {
+            throw new IllegalArgumentException("점수를 입력해주세요.");
+        }
+    }
+
+    public void checkScore() {
+        checkScoreIsWrong();
+    }
+
+    private void checkScoreIsWrong() {
+        if (this.score < MINIMUM_SCORE || this.score > MAX_SCORE) {
+            throw new IllegalArgumentException("잘못된 점수를 입력하셨습니다.");
+        }
+    }
+
+    public boolean isStrike() {
+        return this.score == STRIKE_SCORE;
+    }
+
+    @Override
+    public boolean equals(Object score) {
+        if (this == score) {
+            return true;
+        }
+
+        if (score == null) {
+            return false;
+        }
+
+        return this.score == (int)score;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score);
+    }
+}
---
+++ src/test/java/domain/ScoreGroupTest.java
@@ -0,0 +1,22 @@
+package domain;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ScoreGroupTest {
+
+    @Test
+    public void ScoreGroup_정상값_가져오는지() {
+        Score score = new Score(10);
+        int frameCount = 1;
+
+        ScoreGroup scoreGroup = ScoreGroup.findByScore(frameCount, score);
+        assertThat(scoreGroup).isEqualTo(ScoreGroup.STRIKE);
+    }
+
+    @Test
+    public void ScoreGroup_STRIKE_display_정상_리턴() {
+        assertThat(ScoreGroup.STRIKE.getDisplay()).isEqualTo("X");
+    }
+}
