0f69ede913e2b72c95b987b2876132951dd2cec2
SGKIM94 <tkdrn8578@naver.com>
2019-06-29 16:38:46 +0900

    [MODIFY] Player 객체를 static 에서 non-static 으로 변경하면서 테스트코드 수정

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,24 +1,30 @@
+package domain;
+
 import org.junit.Test;
 
 public class PlayerTest {
+
     @Test
     public void 참가자_이름을_입력받는다() {
         String name = "kim";
+        Player player = new Player(name);
 
-        Player.checkName(name);
+        player.checkName();
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void 참가자_이름이_없는_경우_오류_발생() {
         String name = "";
+        Player player = new Player(name);
 
-        Player.checkName(name);
+        player.checkName();
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void 참가자_이름이_영어_3글자인지_검사() {
         String name = "abcd";
+        Player player = new Player(name);
 
-        Player.checkName(name);
+        player.checkName();
     }
 }
