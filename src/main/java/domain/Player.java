d9ad768e8ce283c7dbb56db6f44500b62d104ada
SGKIM94 <tkdrn8578@naver.com>
2019-06-29 16:33:25 +0900

    [ADD] 참가자 이름을 출력하는 함수 추가

---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,33 @@
+package domain;
+
+public class Player {
+    public static final int MINIMUM_PLAYER_LENGTH = 3;
+    private String player;
+
+    public Player(String player) {
+        this.player = player;
+    }
+
+    public void checkName() {
+        checkPlayerIsNull();
+
+        checkPlayerNameLengthLongerThanThree();
+    }
+
+    private void checkPlayerNameLengthLongerThanThree() {
+        if (this.player.length() > MINIMUM_PLAYER_LENGTH) {
+            throw new IllegalArgumentException("참가자 이름이 너무 깁니다.");
+        }
+    }
+
+    private void checkPlayerIsNull() {
+        if (this.player.isEmpty()) {
+            throw new IllegalArgumentException("잘못된 참가자 이름입니다.");
+        }
+    }
+
+    public void printName() {
+        System.out.print("| " + this.player + "  |");
+
+    }
+}
