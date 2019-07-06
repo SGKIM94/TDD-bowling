5000d28729323968de02de6e97235acaf71b8cf3
SGKIM94 <tkdrn8578@naver.com>
2019-07-06 14:48:23 +0900

    [MODIFY] BallThrowCount 일급 컬렉션을 추가함에 따라 메서드 내 로직 수정

--- src/main/java/domain/Frame.java
+++ src/main/java/domain/Frame.java
@@ -3,29 +3,28 @@ package domain;
 public class Frame {
     public static final int MAX_BALL_THROW_COUNT = 2;
     public static final int MINIMUM_BALL_THROW_COUNT = 0;
-    public static final int ADD_CURRENT_BALL_THROWING = 1;
 
     private Score score;
-    private int ballThrowCount;
+    private BallThrowCount ballThrowCount;
 
-    public Frame(Score score, int ballThrowCount) {
+    public Frame(Score score, BallThrowCount ballThrowCount) {
         this.score = score;
         this.ballThrowCount = ballThrowCount;
     }
 
     public boolean canSkipThisFrame() {
-        ScoreGroup scoreGroup = ScoreGroup.findByScore(this.ballThrowCount + ADD_CURRENT_BALL_THROWING, this.score);
+        ScoreGroup scoreGroup = ScoreGroup.findByScore(this.ballThrowCount, this.score);
 
         return !ScoreGroup.ELSE.equals(scoreGroup);
     }
 
     public void checkBallThrowCount() {
-        if (this.ballThrowCount > MAX_BALL_THROW_COUNT || this.ballThrowCount < MINIMUM_BALL_THROW_COUNT) {
-            throw new IllegalArgumentException();
+        if (this.ballThrowCount.isBigger(MAX_BALL_THROW_COUNT) || this.ballThrowCount.isSmaller(MINIMUM_BALL_THROW_COUNT)) {
+            throw new IllegalArgumentException("볼 던진 횟수의 값이 올바르지 않습니다.");
         }
     }
 
-    public int getBallThrowCount() {
+    public BallThrowCount getBallThrowCount() {
         return this.ballThrowCount;
     }
 }
