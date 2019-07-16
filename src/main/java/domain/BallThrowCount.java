3d564335d76a959f284f1c9845626e56e5af1523
SGKIM94 <tkdrn8578@naver.com>
2019-07-16 23:11:06 +0900

    [ADD] 볼 던진 횟수가 1회인지, 2회인지 확인하는 메서드 추가 및 테스트 코드 추가

--- src/main/java/domain/BallThrowCount.java
+++ src/main/java/domain/BallThrowCount.java
@@ -2,6 +2,8 @@ package domain;
 
 public class BallThrowCount {
     public static final int NEXT_STEP = 1;
+    public static final int FIRST_BALL_THROWING = 1;
+    public static final int SECOND_BALL_THROWING = 2;
     private int ballThrowCount;
 
     public BallThrowCount(int ballThrowCount) {
@@ -31,5 +33,13 @@ public class BallThrowCount {
 
     public int getNextBallCount() {
         return this.ballThrowCount + NEXT_STEP;
+    }
+
+    public boolean isFirstBallThrowing() {
+        return this.ballThrowCount == FIRST_BALL_THROWING;
+    }
+
+    public boolean isSecondBallThrowing() {
+        return this.ballThrowCount == SECOND_BALL_THROWING;
     }
 }
--- src/test/java/domain/BallThrowCountTest.java
+++ src/test/java/domain/BallThrowCountTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class BallThrowCountTest {
     public static final int MINIMUM_BALL_THROW_COUNT = 0;
     public static final int MAX_BALL_THROW_COUNT = 2;
+    final BallThrowCount ballThrowCount = new BallThrowCount(1);
 
     @Test
     public void 점수를_입력받으면_볼카운트는_자동으로_증가한다() {
@@ -14,16 +15,18 @@ public class BallThrowCountTest {
 
     @Test
     public void 볼_던진_횟수의_값을_임의의_값보다_더_큰지_비교한다() {
-        BallThrowCount ballThrowCount = new BallThrowCount(1);
-
         assertThat(ballThrowCount.isBigger(MINIMUM_BALL_THROW_COUNT)).isEqualTo(true);
     }
 
     @Test
     public void 볼_던진_횟수의_값을_임의의_값과_더_작은지_비교한다() {
-        BallThrowCount ballThrowCount = new BallThrowCount(1);
-
         assertThat(ballThrowCount.isSmaller(MAX_BALL_THROW_COUNT)).isEqualTo(true);
     }
 
+    @Test
+    public void 볼_던짓_횟수가_처음인가() {
+        final BallThrowCount ballThrowCount = new BallThrowCount(1);
+
+        assertThat(ballThrowCount.isFirstBallThrowing()).isEqualTo(true);
+    }
 }
