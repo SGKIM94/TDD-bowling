adaa87ed102dd31267e4d6fc297a4cab73c55a9a
SGKIM94 <tkdrn8578@naver.com>
2019-07-17 22:32:59 +0900

    [MODIFY] 프레임이 다음으로 넘어가는지에 대한 테스트코드 추가

--- src/test/java/domain/BawlingGameTest.java
+++ src/test/java/domain/BawlingGameTest.java
@@ -2,12 +2,15 @@ package domain;
 
 import org.junit.Test;
 
+import java.util.Collections;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BawlingGameTest {
     public static final int SCORE = 10;
     public static final int MAX_SCORE = 10;
     final BawlingGame bawlingGame = new BawlingGame();
+    final Scores scores = new Scores(Collections.singletonList(new Score(SCORE)));
 
     @Test
     public void 한_프레임의_최대점수와_점수가_일치하는지_테스트() {
@@ -16,16 +19,14 @@ public class BawlingGameTest {
 
     @Test
     public void 점수를_입력하여_Frame_을_셋팅한다() {
-        Frame frame = bawlingGame.setFramesScore(new Score(SCORE));
+        Frame frame = bawlingGame.setFramesScore(scores);
 
         assertThat(frame).isNotNull();
     }
 
     @Test
     public void 프레임이_넘어가도디는지_확인해서_넘어갈_수_있다면_Fame_라운드를_늘린다() {
-        Score score = new Score(SCORE);
-
-        bawlingGame.setFramesScore(score);
+        bawlingGame.setFramesScore(scores);
 
         assertThat(bawlingGame.checkFrameStepOverNext()).isEqualTo(true);
     }
