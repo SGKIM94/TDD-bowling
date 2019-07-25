7b514899fa203ec5f5d8aa3687684e57192d0cba
SGKIM94 <tkdrn8578@naver.com>
2019-07-25 22:28:24 +0900

    [REFACTORING] BawlingGame 주석 제거 및 소스 정리

--- src/main/java/controller/BawlingGame.java
+++ src/main/java/controller/BawlingGame.java
@@ -38,14 +38,12 @@ public class BawlingGame {
         makeFramesFormat(thirdScore, player, ballThrowCount, "3");
 
         //TODO : ballThrowCount List<Integer> 로 일급 콜렉션으로 만들기
-        //TODO : 프레임을 넘어가지 못했을 때 출력 값을 한 프레임 안에 출력할 수 있도록 하기 (example : 8 / X)
         //TODO : controller 중복 제거하기 (함수형으로 변경)
         //TODO : 람다와 스트림으로 변경할 수 있는 것 변경하기
         //TODO : Frame 에 몇 카운트 인지 넣기
         //TODO : 점수를 입력할 때 넘어갈 수 있는지 계속 확인
         // 넘어갈 수 있다면 넘기고 못넘어가면 넘기지 말기
         // ballCount 를 수동으로 더해주지 않아도 자동으로 증가할 수 있도록 해야할듯
-        // 일급콜렉션화 시켜서 하면 될듯
         // 람다와 스트림화 시키기
         // 출력하기 전에 넘어갈 수 있는지 검사하고 출력할 때 라운드를 보고 출력해야할 듯
 
