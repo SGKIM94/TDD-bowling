fdbb14299dfd80fbae82c8cf725927f20d449713
SGKIM94 <tkdrn8578@naver.com>
2019-08-12 17:59:56 +0900

    [REFACTORING] 반복코드에 대한 멤버변수 추가에 따른 인자 제거 및 메서드 체이닝 형태로 수정
     * 매 라운드마다 반복되는 중복코드를 메서드로 중복 제거
     * 많은 인자가 발생하여 멤버변수 추가 후 this를 리턴하여 메서드 체이닝 형태로 수정

--- src/main/java/controller/BawlingGame.java
+++ src/main/java/controller/BawlingGame.java
@@ -5,63 +5,49 @@ import view.BallingFrame;
 import view.NameFrame;
 
 class BawlingGame {
-    static void main() {
+    private ScoreDisplays scoreDisplays;
+    private Player player;
+    private Frames frames;
+    private Scores scores;
+
+
+    BawlingGame(String playerNmae) {
         System.out.println("플레이어 이름은(3 english letters)?: ");
-        Player player = new Player("KSG");
 
-        Frames frames = new Frames();
-
-        BallingFrame.printNameFrame();
-        NameFrame.printNameFrame(player);
-
-        BallingFrame.printEmptyFrames(10);
-
-        ScoreDisplays scoreDisplays = new ScoreDisplays();
-
-        Scores scores = new Scores();
-
-        Score firstScore = new Score(10);
-        scores.add(firstScore);
-        scoreDisplays = frames.makeScoreDisplayForm(firstScore, scoreDisplays);
-
-        printCurrentFrames(firstScore, player, scoreDisplays, "1");
-
-        Score secondScore = new Score(8);
-        scores.add(secondScore);
-        scoreDisplays = frames.makeScoreDisplayForm(secondScore, scoreDisplays);
-
-        printCurrentFrames(secondScore, player, scoreDisplays, "2");
-
-        Score thirdScore = new Score(2);
-        scores.add(thirdScore);
-        scoreDisplays = frames.makeScoreDisplayForm(thirdScore, scoreDisplays);
-
-        printCurrentFrames(thirdScore, player, scoreDisplays, "3");
-
-        Score fourthScore = new Score(5);
-        scores.add(fourthScore);
-        scoreDisplays = frames.makeScoreDisplayForm(fourthScore, scoreDisplays);
-
-        printCurrentFrames(fourthScore, player, scoreDisplays, "4");
-
-        Score fifthScore = new Score(3);
-        scores.add(fifthScore);
-        scoreDisplays = frames.makeScoreDisplayForm(fifthScore, scoreDisplays);
-
-        printCurrentFrames(fifthScore, player, scoreDisplays, "5");
-
-        Score sixScore = new Score(10);
-        scores.add(sixScore);
-        scoreDisplays = frames.makeScoreDisplayForm(sixScore, scoreDisplays);
-
-        printCurrentFrames(sixScore, player, scoreDisplays, "6");
+        this.player = new Player(playerNmae);
+        this.frames = new Frames();
+        this.scoreDisplays = new ScoreDisplays();
+        this.scores = new Scores();
     }
 
-    private static void printCurrentFrames(Score score, Player player, ScoreDisplays scoreDisplays, String turnNumber) {
+    void start() {
+        BallingFrame.printNameFrame();
+        NameFrame.printNameFrame(this.player);
+        BallingFrame.printEmptyFrames(10);
+
+        bowl(10, "1")
+                .bowl(8, "2")
+                .bowl(2, "2")
+                .bowl(5, "4")
+                .bowl(3, "5")
+                .bowl(10, "6");
+    }
+
+    private BawlingGame bowl(int score, String turnNumber) {
+        Score firstScore = new Score(score);
+        scores.add(firstScore);
+        this.scoreDisplays = frames.makeScoreDisplayForm(firstScore, scoreDisplays);
+
+        printCurrentFrames(firstScore, scoreDisplays, turnNumber);
+
+        return this;
+    }
+
+    private void printCurrentFrames(Score score, ScoreDisplays scoreDisplays, String turnNumber) {
         System.out.println(turnNumber + "프레임 투구 : " + score.toString());
 
         BallingFrame.printNameFrame();
-        NameFrame.printNameFrame(player);
+        NameFrame.printNameFrame(this.player);
         BallingFrame.printFrames(10, scoreDisplays);
     }
 }
