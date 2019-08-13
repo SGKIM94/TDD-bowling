1efac47a7b2d528866fb859ab54862aa90ad855b
SGKIM94 <tkdrn8578@naver.com>
2019-08-13 22:57:31 +0900

    [MODIFY] 점수판을 출력하기 위한 view 클래스 추가

---
+++ src/main/java/view/ScoreFrame.java
@@ -0,0 +1,38 @@
+package view;
+
+import domain.Frame;
+import domain.Frames;
+
+public class ScoreFrame {
+    public static void printScoreFrames(int frameTotalCount, Frames frames) {
+        int framesSize = frames.getFramesSize();
+        for (int i = 0; i < framesSize; i++) {
+            Frame frame = frames.get(i);
+            makeFrameWhenExistScores(frame);
+        }
+
+        int remainFrameCount = frameTotalCount - framesSize;
+        for (int i = framesSize; i < remainFrameCount; i++) {
+            printEmptyFrame();
+        }
+
+        printEdgeFrame();
+    }
+
+    private static void makeFrameWhenExistScores(Frame frame) {
+        if (frame.getScoreSize() == 0) {
+            System.out.print("|    |");
+            return;
+        }
+
+        System.out.print("   " + frame.getSumScore().toString() + "  |");
+    }
+
+    private static void printEmptyFrame() {
+        System.out.print("      |");
+    }
+
+    private static void printEdgeFrame() {
+        System.out.println("      |");
+    }
+}
