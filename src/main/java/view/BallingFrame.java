8505d464e2a6ced38d50cdc4eaaf81ce3909156d
SGKIM94 <tkdrn8578@naver.com>
2019-08-17 17:20:04 +0900

    [REFACTORING] printFrame 에 중복되는 remainFrame 출력 로직을 메서드로 만들어 통일화 작업

--- src/main/java/view/BallingFrame.java
+++ src/main/java/view/BallingFrame.java
@@ -2,7 +2,10 @@ package view;
 
 import domain.ScoreDisplays;
 
+import static view.ScoreFrame.FRAME_TOTAL_SIZE;
+
 public class BallingFrame {
+
     public static void printNameFrame() {
         System.out.println("| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |");
     }
@@ -11,22 +14,46 @@ public class BallingFrame {
         makeEmptyFrameFormat(emptyFrameCount);
     }
 
-    public static void printFrames(int frameTotalCount, ScoreDisplays scoreDisplays) {
+    public static void printFrames(ScoreDisplays scoreDisplays) {
         int displaysSize = scoreDisplays.displaysSize();
         for (int i = 0; i < displaysSize; i++) {
             makeFrameWhenExistScoreDisplay(scoreDisplays.get(i));
         }
 
-        int remainFrameCount = frameTotalCount - displaysSize;
-        for (int i = displaysSize; i < remainFrameCount; i++) {
-            printEmptyFrame();
-        }
-
+        printRemainEmptyFrame(displaysSize);
         printEdgeFrame();
     }
 
+    static void printRemainEmptyFrame(int size) {
+        int remainFrameCount = FRAME_TOTAL_SIZE - size - 1;
+
+        for (int i = 0; i < remainFrameCount; i++) {
+            printEmptyFrame();
+        }
+    }
+
     private static void makeFrameWhenExistScoreDisplay(String scoreDisplay) {
+        int scoreDisplaySize = scoreDisplay.length();
+
+        if (isTwoDigitScoreDisplay(scoreDisplaySize)) {
+            System.out.print("  " + scoreDisplay + "  |");
+            return;
+        }
+
+        if (isThreeDigitScoreDisplay(scoreDisplaySize)) {
+            System.out.print("  " + scoreDisplay + " |");
+            return;
+        }
+
         System.out.print("   " + scoreDisplay + "  |");
+    }
+
+    private static boolean isThreeDigitScoreDisplay(int scoreDisplaySize) {
+        return scoreDisplaySize == 3;
+    }
+
+    private static boolean isTwoDigitScoreDisplay(int scoreDisplaySize) {
+        return scoreDisplaySize == 2;
     }
 
     private static void makeEmptyFrameFormat(int emptyFrameCount) {
