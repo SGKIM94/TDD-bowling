2236604197c77d9fc15aa0d6027e3f5d026756df
SGKIM94 <tkdrn8578@naver.com>
2019-08-17 17:28:06 +0900

    [REFACTORING] if 문의 조건문을 함수화로 변경

--- src/main/java/domain/Scores.java
+++ src/main/java/domain/Scores.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 public class Scores {
     private static final int FIRST_INDEX = 0;
+    private static final int ONE_BALL_THROW_COUNT = 1;
+    private static final int FIRST_BAWL_SCORE = 0;
+    private static final int SECOND_BAWL_SCORE = 1;
 
     private List<Score> scores;
 
@@ -27,16 +30,19 @@ public class Scores {
 
     public Scores add(Score score) {
         this.scores.add(score);
-
         return this;
     }
 
     Score getSumScores() {
-        if (this.scores.size() == 1) {
-            return this.scores.get(0);
+        if (isOneBallThrowCount()) {
+            return this.scores.get(FIRST_BAWL_SCORE);
         }
 
-        return this.scores.get(0).sumScores(this.scores.get(1));
+        return this.scores.get(FIRST_BAWL_SCORE).sumScores(this.scores.get(SECOND_BAWL_SCORE));
+    }
+
+    private boolean isOneBallThrowCount() {
+        return this.scores.size() == ONE_BALL_THROW_COUNT;
     }
 
     int getScoresSize() {
