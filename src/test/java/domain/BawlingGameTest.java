b14e7d48245e139afc3e1a35ac2ccb7c5c48b1f1
SGKIM94 <tkdrn8578@naver.com>
2019-08-22 22:27:42 +0900

    [MODIFY] 테스트코드에 작성된 불필요한 상수값들 제거

--- src/test/java/domain/BawlingGameTest.java
+++ src/test/java/domain/BawlingGameTest.java
@@ -6,8 +6,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BawlingGameTest {
     private static final int SCORE = 10;
-    private static final int MAX_SCORE = 10;
-    final Scores scores = new Scores(new Score(SCORE));
 
     @Test
     public void 이전의_점수와_새로운_점수를_더해_총_쩜수를_구한다() {
