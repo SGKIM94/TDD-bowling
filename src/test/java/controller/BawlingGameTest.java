2a4581a8bc76f1a23e6de61d61063a2df89d281d
SGKIM94 <tkdrn8578@naver.com>
2019-08-24 16:37:47 +0900

    [REFACTORING] 불필요한 상수값들 제거

--- src/test/java/controller/BawlingGameTest.java
+++ src/test/java/controller/BawlingGameTest.java
@@ -6,7 +6,6 @@ public class BawlingGameTest {
     @Test
     public void mainTest() {
         BawlingGame bawlingGame = new BawlingGame("kim");
-
         bawlingGame.bowlingGameStart();
     }
 }
--- src/test/java/domain/BawlingGameTest.java
+++ src/test/java/domain/BawlingGameTest.java
@@ -5,8 +5,6 @@ import org.junit.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BawlingGameTest {
-    private static final int SCORE = 10;
-
     @Test
     public void 이전의_점수와_새로운_점수를_더해_총_쩜수를_구한다() {
         BawlingGame bawlingGame = new BawlingGame(new Score(10));
